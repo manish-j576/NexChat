7c81bed3a8a7c66653773bb3c39e97d3c4456f09
Manish Joshi <manishjoshi705572@gmail.com>
2025-12-25 13:40:39 +0530

    Fixed prisma and tailwind error

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-blue-200 text-4xl">
       we have to navigate this to the /logi
 
     </div>
---
+++ prisma/migrations/20251225080027_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "USER" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "USER_pkey" PRIMARY KEY ("id")
+);
